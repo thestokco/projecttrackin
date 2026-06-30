491f4ea54178917a3e4e826834de53395d488f95
thestokco <sales@theshluxe.com>
2026-06-30 19:49:00 +0800

    Force service worker cache bust to serve latest OCR fix

--- public/sw.js
+++ public/sw.js
@@ -1,9 +1,14 @@
+// v2 - force cache bust
 self.addEventListener("install", (e) => {
   self.skipWaiting();
 });
 
 self.addEventListener("activate", (e) => {
-  e.waitUntil(clients.claim());
+  e.waitUntil(
+    caches.keys().then((names) =>
+      Promise.all(names.map((name) => caches.delete(name)))
+    ).then(() => clients.claim())
+  );
 });
 
 self.addEventListener("fetch", (e) => {
